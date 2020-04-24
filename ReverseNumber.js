a718ae53252cc288e7b8064e9796fce9751f2c0f
landing-labzz <35526909+landing-labzz@users.noreply.github.com>
2020-04-24 15:40:54 +0300

    Update and rename index.js to ReverseNumber.js

--- ReverseNumber.js
+++ ReverseNumber.js
@@ -1,31 +1,21 @@
 var num = 5921;
-
 function getReversedNum(num) {
   let result = 0;
   while (num) {
     result = result * 10 + num % 10;
     num = Math.floor(num / 10);
   }
-
   return result;
 }
-
-console.log(getReversedNum(num));
-
-
-
+// console.log(getReversedNum(num));
 
 var number = 76543;
-
 function reverse(number) {
     let res = 0;
-
     while (number){
         res = res * 10 + number % 10;
-        number = Math.floor(number / 10)
-        
+        number = Math.floor(number / 10)      
     }
     return res
 }
-
-console.log(reverse(number));
+// console.log(reverse(number));
